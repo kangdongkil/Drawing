1178b8a1c3a8e17639fdd7d891d1ed6f22ee5b47
kangdongkil <kbk3418@naver.com>
2014-09-10 16:07:05 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup(){
 	createCanvas(640, 480);
   	background(0);
-  	stroke(255,100,100);
+  	stroke(255,0,0);
  	noFill();
   	line(10, 10, 90, 10);
-    rect(20, 20, 80, 50);
-    ellipse(50, 50, 50, 50);
+    	rect(20, 20, 80, 50);
+    	ellipse(50, 50, 50, 50);
 }
