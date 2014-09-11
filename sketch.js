435001d9ddc6ee91dd04b96880da5299eeb804aa
kangdongkil <kbk3418@naver.com>
2014-09-11 13:39:24 +0900

    sujung

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 function setup(){
-	createCanvas(640, 480);
-  	background(0);
+	createCanvas(680, 480);
+  	//0:black, 255:white
+  	//background(red,green.blue);
+  	background(255,0,0);
   	stroke(255);
  	noFill();
-  	line(10, 10, 90, 10);
-    rect(20, 20, 80, 50);
-    ellipse(50, 50, 50, 50);
+
+	line(10,10,90,10);
+	rect(20,20,100,100);
+	ellipse(320,240,100,50);
+
+
 }
